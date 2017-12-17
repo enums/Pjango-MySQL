46a0ad0a7946378da4fa10535e1299d6f10428af
zhengyuqi <zhengyuqi@baidu.com>
2017-12-18 00:07:03 +0800

    update to Perfect 3.x

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
     ]
 )
