9f2f365dc5098cf16739be6a5561f211b888df6f
Enum <enum@enumsblog.com>
2017-12-23 16:49:52 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Pjango-MySQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
     ]
 )
