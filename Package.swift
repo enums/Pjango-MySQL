93eef06fd38a40cabd06efce0b6f44db63b7ed56
zhengyuqi <zhengyuqi@baidu.com>
2018-01-18 19:03:21 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,26 @@
 import PackageDescription
 
+#if os(OSX)
 let package = Package(
     name: "Pjango-MySQL",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
-    ]
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3, minor: 0),
+        ]
 )
+#else
+let package = Package(
+    name: "Pjango-MySQL",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
+        ]
+)
+#endif
+
+
+
+
+
